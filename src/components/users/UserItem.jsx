afc11d1da2b78c8d9620cfab70c8d5cdc339d256
petar-dobrev <92185890+petar-dobrev@users.noreply.github.com>
2022-03-19 14:47:03 +0200

    Add Context and Reducer for users data

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom"
 
 function UserItem({user : {login, avatar_url}}) {
   return (
-    <div className="card shadow-md compact side bg-base-100">
+    <aricle className="card shadow-md compact side bg-base-100">
         <div className="flex-row items-center space-x-4 card-body">
-            <div>
+            <section>
                 <div className="avatar">
                     <div className="rounded-full shadow w-14 h-14">
                         <img src={avatar_url} alt="User Avatar" />
                     </div>
                 </div>
-            </div>
-            <div>
+            </section>
+            <section>
                 <h2 className="card-title">{login}</h2>
                 <Link className="text-base-content text-opacity-40" to={`/users/${login}`}>Visit Profile</Link>
-            </div>
+            </section>
         </div>
-    </div>
+    </aricle>
   )
 }
 
---
+++ src/context/github/GitHubContext.js
@@ -0,0 +1,44 @@
+import { createContext, useReducer } from "react";
+import gitHubReducer from "./GitHubReducer";
+
+const GitHubContext = createContext();
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+export const GitHubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+
+  const [state, dispatch] = useReducer(gitHubReducer, initialState);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
+  };
+
+  return (
+    <GitHubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
+      }}
+    >
+      {children}
+    </GitHubContext.Provider>
+  );
+};
+
+export default GitHubContext;
