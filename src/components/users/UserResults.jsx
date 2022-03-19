ffcb1bd48cdcc01c36d2322760457218c8533de7
petar-dobrev <92185890+petar-dobrev@users.noreply.github.com>
2022-03-19 15:27:10 +0200

    Add User Search component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,16 +1,10 @@
-import { useEffect, useContext } from "react"
+import { useContext } from "react"
 import Spinner from "../layout/Spinner"
 import UserItem from "./UserItem"
 import GitHubContext from "../../context/github/GitHubContext"
 
 function UserResults() {
-  const {users, loading, fetchUsers} = useContext(GitHubContext)
-
-
-  useEffect(() => {
-      fetchUsers()
-  },[])
-
+  const {users, loading} = useContext(GitHubContext)
 
 
   if(!loading) {
